ce9e302713d232b3677cb667b99cb875407d7548
moutazmahmoud <moutaz.prof.egy@gmail.com>
2024-05-25 14:19:31 +0300

    Refactor: task 4

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,35 +11,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
-    unsigned int i;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
-
-    current = *head;
-
-    if (index == 0)
-    {
-        *head = current->next;
-        if (current->next != NULL)
-            current->next->prev = NULL;
-        free(current);
-        return (1);
-    }
-
-    for (i = 0; current != NULL && i < index; i++)
-        current = current->next;
-
-    if (current == NULL)
-        return (-1);
-
-    if (current->next != NULL)
-        current->next->prev = current->prev;
-
-    if (current->prev != NULL)
-        current->prev->next = current->next;
-
-    free(current);
-    return (1);
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (index == 0)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
+	return (1);
 }
